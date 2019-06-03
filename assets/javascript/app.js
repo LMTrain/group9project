9d0963f07f86b8c8dd1418b1ff909caee900a7f9
LM Train <layconmm@msn.com>
2019-06-03 02:02:36 -0500

    Modify html, js, and css file to reflect changes in line of codes that allow items entered to be stored on FireBase and then render it as a button on the screen for users to use in the Future

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,13 @@
 
   // Initial array of items
   var items = ['Adapters', 'Allergy Medicines', 'Bandana', 'Barrettes', 'Belt', 'Blanket', 'Bobby Pins', 'Book', 'Camera', 'Carry-On', 'Charger', 'Comb', 'Deodorant', 'Duffel Bag', 'Ear Plugs', 'Electric Converters', 'E-reader', 'Eye Drops', 'Eye Mask', 'Face lotion with SPF', 'Face Wash', 'First Aid Kit', 'Flash Light', 'Fleece', 'Floss', 'Hair Brush', 'Hair Conditioner', 'Hair Shampoo', 'Hair Ties', 'Hand Sanitizer', 'Hat', 'Insect Repellent', 'iPad', 'Language Guides', 'Laptop', 'Laxative Medicines', 'Lip Balm', 'Maps', 'Moisturizer', 'Moleskin', 'Mouthwash', 'Nail clippers', 'Padlocks', 'Rain Jacket', 'Rolling Luggage', 'Scarf', 'Scissors', 'Shaving Kit', 'Shorts', 'Sleepwear', 'Socks', 'Sun Visor', 'Sunburn Relief', 'Sunglasses', 'Sunscreen', 'Thermometer', 'Toothbrush', 'Toothpaste', 'Travel Backpack', 'Travel Guides', 'Travel Pillow', 'Travel Towel', 'Tweezers', 'Umbrella', 'Underwear', 'Wheeled Backpack', 'Windbreaker'];
-
+  var cities = [];
   // displayitemInfo function re-renders the HTML to display the appropriate content
   function displayitemInfo() {
 
     var item = $(this).attr("data-name");
     var queryURL = "assets/javascript/get_response.php?query="+item;
-    // var queryURL = "http://api.walmartlabs.com/v1/items/206672856?apiKey=vng9pukufs97mcyyjs5ps266";
-    // Creating an AJAX call for the specific item button being clicked
+    
     $.ajax({
       url: queryURL,
       method: "GET"
@@ -30,8 +29,7 @@
             var image = $("<img>");
             var cR = $("<p>").text("Ratings: " + results[i].customerRating);
             image.attr("src", results[i].thumbnailImage); 
-            image.attr("data-still", results[i].thumbnailImage);
-            // image.attr("data-animate", results[i].images.fixed_height_downsampled.url);
+            image.attr("data-still", results[i].thumbnailImage);            
             image.attr("data-state", "still");
             image.on("click", function () {          
              
@@ -61,72 +59,67 @@
     }
   }
 
-  
-  $("#add-item").on("click", function(event) {
+  var item = "";
+  $("#add-item").click(function (event) {
     event.preventDefault();
     
-    var item = $("#item-input").val().trim();
-    items.push(item);
-   
-    renderButtons();
-    // Clear the textbox when done
+    item = $("#item-input").val().trim();
+    
+  //   // Clear the textbox when done
     $("#item-input").val("");
   });
   
   $(document).on("click", ".item-btn", displayitemInfo);
  
   renderButtons();
-
-  var config = {
-    apiKey: "AIzaSyAzNfLsyx0G7i0wRGEVn5vY65NPHSS2q4A",
-    authDomain: "groupproject1-66056.firebaseapp.com",
-    databaseURL: "https://groupproject1-66056.firebaseio.com/",
-    projectId: "groupproject1-66056",
-    storageBucket: "groupproject1-66056.appspot.com",
-    messagingSenderId: "771982143498",
-    appId: "1:771982143498:web:f5ed32150586ee6e"
-};
-firebase.initializeApp(config);
-// puts the firebase into a variable
-var database = firebase.database();
+  
 
 
-// Initial Values
-var location = "";
-var item = "";
+  var firebaseConfig = {
+    apiKey: "AIzaSyCOWrgjPw8Ov5oER_PXzuHG-MmRPdW9zmQ",
+    authDomain: "group9project-d9aae.firebaseapp.com",
+    databaseURL: "https://group9project-d9aae.firebaseio.com",
+    projectId: "group9project-d9aae",
+    storageBucket: "group9project-d9aae.appspot.com",
+    messagingSenderId: "615277938136",
+    appId: "1:615277938136:web:789dda90e8606a6e"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  // puts the firebase into a variable
+  var database = firebase.database();
 
-$("#add-location").click(function (event) {
+
+  // Initial Values
+  var city = ""; 
+
+  $("#add-city").click(function (event) {
   event.preventDefault();
 
-  var location = $("#location-input").val().trim();
-  var item = $("#item-input").val().trim();
+  city = $("#city-input").val().trim();  
   
   var newItem = {
-  location: location,
+  city: city,
   item: item,
-  
+
   };
 
-  // Uploads train data to the database
+  // Uploads data to the database
   database.ref().push(newItem);
-  
-  $("#location-input").val("");
-  $("#item-input").val("");
-  
-});
 
-database.ref().on("child_added", function(childSnapshot) {
+  $("#city-input").val("");
+  $("#item-input").val("");
+
+  });
+
+  database.ref().on("child_added", function(childSnapshot) {
   console.log(childSnapshot.val());
 
-  var location = childSnapshot.val().name;
-  var item = childSnapshot.val().role;
+  var city = childSnapshot.val().city;
+  var item = childSnapshot.val().item;
 
-  var newRow = $("<tr>").append(
-    $("<td>").text(location),
-    $("<td>").text(item),
-      
-  );
+  items.push(item);
+  cities.push(city);
+  renderButtons();
 
-  // Append the new row to the table
-  $("table > tbody").append(newRow);
-});
+  });
