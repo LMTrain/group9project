1a8278056dee957af49ff9ec560159d3f4368c7b
AaronO <a.odland@hotmail.com>
2019-06-04 15:12:01 -0500

    Update app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,7 @@
 
   // Initial array of items
-
   var items = ['Adapters', 'Allergy Medicines', 'Bandana', 'Barrettes', 'Belt', 'Blanket', 'Bobby Pins', 'Book', 'Camera', 'Carry-On', 'Charger', 'Comb', 'Deodorant', 'Duffel Bag', 'Ear Plugs', 'Electric Converters', 'E-reader', 'Eye Drops', 'Eye Mask', 'Face lotion with SPF', 'Face Wash', 'First Aid Kit', 'Flash Light', 'Fleece', 'Floss', 'Hair Brush', 'Hair Conditioner', 'Hair Shampoo', 'Hair Ties', 'Hand Sanitizer', 'Hat', 'Insect Repellent', 'iPad', 'Language Guides', 'Laptop', 'Laxative Medicines', 'Lip Balm', 'Maps', 'Moisturizer', 'Moleskin', 'Mouthwash', 'Nail clippers', 'Padlocks', 'Rain Jacket', 'Rolling Luggage', 'Scarf', 'Scissors', 'Shaving Kit', 'Shorts', 'Sleepwear', 'Socks', 'Sun Visor', 'Sunburn Relief', 'Sunglasses', 'Sunscreen', 'Thermometer', 'Toothbrush', 'Toothpaste', 'Travel Backpack', 'Travel Guides', 'Travel Pillow', 'Travel Towel', 'Tweezers', 'Umbrella', 'Underwear', 'Wheeled Backpack', 'Windbreaker'];
-  var cities = [];
-
+  var cities = ['St Paul', 'Minnapolis', "Roseville",'Maplewood', 'Andover', 'Forest Lake'];
   // displayitemInfo function re-renders the HTML to display the appropriate content
   function displayitemInfo() {
 
@@ -44,6 +42,7 @@
         }      
     });
   }
+  
   // Function for displaying item data
   function renderButtons() {
 
@@ -60,19 +59,19 @@
       $("#buttons-view").append(a);
     }
   }
-  function renderButtons1() {
+  function renderCities() {
 
     
-    $("#buttons-view1").empty();
+    $("#Loaction-place").empty();
 
     // Looping through the array of items
-    for (var i = 0; i < locations.length; i++) {
+    for (var i = 0; i < cities.length; i++) {
       
       var a = $("<button>");      
-      a.addClass("item-btn");      
-      a.attr("data-name", locations[i]);      
-      a.text(locations[i]);     
-      $("#buttons-view1").append(a);
+      a.addClass("city-btn");      
+      a.attr("data-name", cities[i]);      
+      a.text(cities[i]);     
+      $("#Loaction-place").append(a);
     }
   }
 
@@ -89,9 +88,9 @@
   $(document).on("click", ".item-btn", displayitemInfo);
  
   renderButtons();
+  renderCities(); 
 
   
-
 
   var firebaseConfig = {
     apiKey: "AIzaSyCOWrgjPw8Ov5oER_PXzuHG-MmRPdW9zmQ",
@@ -139,5 +138,49 @@
   items.push(item);
   cities.push(city);
   renderButtons();
+  // renderCities();
 
   });
+  var initMap = function() {
+    var provider = new com.modestmaps.TemplatedLayer('http://tile.openstreetmap.org/{Z}/{X}/{Y}.png');
+    var map = new com.modestmaps.Map('map', provider); 
+    var canvas = document.createElement('canvas');
+    canvas.style.position = 'absolute';
+    canvas.style.left = '0';
+    canvas.style.top = '0';
+    canvas.width = map.dimensions.x;
+    canvas.height = map.dimensions.y;
+    map.parent.appendChild(canvas);
+  
+    var locations = [];
+    var sf = new com.modestmaps.Location(37.7749295, -122.4194155);
+    var london = new com.modestmaps.Location(51.5001524, -0.1262362);
+    for (var i = 0; i <= 100; i++) {
+      var f = i/100.0;
+      locations.push(com.modestmaps.Location.interpolate(sf, london, f));
+    }
+    map.setExtent(locations);
+  
+    // function redraw() {
+    //   var ctx = canvas.getContext('2d');
+    //   ctx.clearRect(0,0,canvas.width,canvas.height);
+    //   ctx.strokeStyle = '#404040';
+    //   ctx.beginPath();
+    //   var p = map.locationPoint(locations[0]);
+    //   ctx.moveTo(p.x,p.y);
+    //   for (var i = 1; i < locations.length; i++) {
+    //     p = map.locationPoint(locations[i]);
+    //     ctx.lineTo(p.x,p.y);
+    //   }
+    //   ctx.stroke();
+    // }
+  
+    // map.addCallback('drawn', redraw);
+    // map.addCallback('resized', function() {
+    //   canvas.width = map.dimensions.x;
+    //   canvas.height = map.dimensions.y;
+    //   redraw();
+    // });
+  
+    // redraw();
+  }
